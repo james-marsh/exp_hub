53f796543133e3cdda8f1207b7132bf5cfed2abb
james-marsh <james.d.marsh@gmail.com>
2014-08-13 00:32:07 -0500

    Additional cleanup
    Removed unused namespaces and redundant code in the app_start,
    controllers, and views

--- EXPLOSION_HUB/EXPLOSION_HUB/App_Start/RouteConfig.cs
+++ EXPLOSION_HUB/EXPLOSION_HUB/App_Start/RouteConfig.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace EXPLOSION_HUB
+namespace EXPLOSION_HUB.App_Start
 {
 	public class RouteConfig
 	{
@@ -13,10 +9,7 @@ namespace EXPLOSION_HUB
 		{
 			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-			routes.MapRoute(
-				name: "Default",
-				url: "{controller}/{action}/{id}",
-				defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+			routes.MapRoute("Default", "{controller}/{action}/{id}", new { controller = "Home", action = "Index", id = UrlParameter.Optional }
 			);
 		}
 	}
